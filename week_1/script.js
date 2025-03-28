2f040b187bbb6d1737498314e3dab284a959d9e0
bean-123 <amy.platt@hotmail.com>
2025-03-28 13:06:03 +0200

    testi

---
+++ week_1/script.js
@@ -0,0 +1,13 @@
+console.log("Tämä on Javascript-testi.");
+alert("Hi");
+
+function funktio() {
+  let x, y, z;
+  x = 5;
+  y = 10;
+  z = x + y;
+
+  console.log("This is the value of z: ", z);
+}
+
+funktio();
